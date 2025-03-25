bbe40150bb267ce50310304b58ac3bd09e512afb
khim7mimi <6733171021@student.chula.ac.th>
2025-03-25 12:32:59 +0700

    edit env

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/v1/bookings', bookings);
 
 const PORT = process.env.PORT || 5003;
 
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'on ',process.env.HOST,' :', PORT));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err,promise) => {
